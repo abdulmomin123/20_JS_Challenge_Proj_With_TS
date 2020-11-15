9f3afd9f260941873f1f25b0a35471c223eaaca3
Abdul Momin <abdulmomin114477@gmail.com>
2020-11-15 22:48:56 +0600

    all

--- Form_Validator/dist/app.js
+++ Form_Validator/dist/app.js
@@ -8,16 +8,33 @@ const elements = {
 };
 const checkField = (field) => {
     if (field === 'userName') {
+        if (elements.username.value.length > 2)
+            return true;
+        return false;
     }
     else if (field === 'email') {
+        if (elements.email.value.includes('@'))
+            return true;
+        return false;
     }
     else if (field === 'password') {
+        if (elements.password.value.length > 5)
+            return true;
+        return false;
     }
     else if (field === 'confirmPassword') {
+        if (elements.password2.value.length > 5 &&
+            elements.password2.value === elements.password.value)
+            return true;
+        return false;
     }
+    return;
 };
 const validateForm = (e) => {
     e.preventDefault();
-    checkField('userName');
+    console.log(checkField('userName'));
+    checkField('email');
+    checkField('password');
+    checkField('confirmPassword');
 };
 elements.form.addEventListener('submit', validateForm);
--- Form_Validator/src/app.ts
+++ Form_Validator/src/app.ts
@@ -11,19 +11,42 @@ const elements = {
 const checkField = (
   field: 'userName' | 'email' | 'password' | 'confirmPassword'
 ) => {
-  //
+  if (field === 'userName') {
+    if (elements.username.value.length > 2) return true;
+    return false;
+    //   'Username should be greater than 3 characters';
+  } else if (field === 'email') {
+    if (elements.email.value.includes('@')) return true;
+    return false;
+  } else if (field === 'password') {
+    if (elements.password.value.length > 5) return true;
+    return false;
+  } else if (field === 'confirmPassword') {
+    if (
+      elements.password2.value.length > 5 &&
+      elements.password2.value === elements.password.value
+    )
+      return true;
+    return false;
+  }
+
+  return;
 };
 
 const validateForm = (e: Event) => {
   e.preventDefault();
 
   // check username
+  console.log(checkField('userName'));
 
   // check email
+  checkField('email');
 
   // check password
+  checkField('password');
 
   // check confirm password
+  checkField('confirmPassword');
 };
 
 // Event listeners
