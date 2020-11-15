a7d56768ad23888dd578d27d1a61747f10b2bb7e
Abdul Momin <abdulmomin114477@gmail.com>
2020-11-15 20:59:45 +0600

    all

--- Form_Validator/dist/app.js
+++ Form_Validator/dist/app.js
@@ -1,4 +1,11 @@
 "use strict";
+const elements = {
+    form: document.querySelector('#form'),
+    username: document.querySelector('#username'),
+    email: document.querySelector('#email'),
+    password: document.querySelector('#password'),
+    password2: document.querySelector('#password2'),
+};
 const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
--- Form_Validator/src/app.ts
+++ Form_Validator/src/app.ts
@@ -1,3 +1,12 @@
+// DOM Elements
+const elements = {
+  form: document.querySelector('#form'),
+  username: document.querySelector('#username'),
+  email: document.querySelector('#email'),
+  password: document.querySelector('#password'),
+  password2: document.querySelector('#password2'),
+};
+
 const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
