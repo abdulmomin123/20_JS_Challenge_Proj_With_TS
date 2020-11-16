aa9827c694047544489bd3c2918dedb81ca03857
Abdul Momin <abdulmomin114477@gmail.com>
2020-11-16 08:15:31 +0600

    all

---
+++ movie-seat-booking/app.js
@@ -0,0 +1,6 @@
+"use strict";
+var container = document.querySelector('.container');
+var seats = document.querySelectorAll('.row .seat:not(.occupied)');
+var count = document.getElementById('count');
+var total = document.getElementById('total');
+var movieSelect = document.getElementById('movie');
