81cfeec98ccf727d43cce73b5ca329c84c46893a
Abdul Momin <abdulmomin114477@gmail.com>
2020-11-16 10:05:22 +0600

    all

---
+++ custom-video-player/app.js
@@ -0,0 +1,6 @@
+"use strict";
+const video = document.getElementById('video');
+const playVideo = document.getElementById('play');
+const stopVideo = document.getElementById('stop');
+const progress = document.getElementById('progress');
+const timestamp = document.getElementById('timestamp');
