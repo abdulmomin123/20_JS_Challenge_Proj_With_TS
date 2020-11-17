55277af5bee4fd5fbef1d56852af845fcb6d1d84
Abdul Momin <abdulmomin114477@gmail.com>
2020-11-17 21:49:27 +0600

    all

--- exchange-rate/app.js
+++ exchange-rate/app.js
@@ -1,9 +1,20 @@
 "use strict";
 const elements = {
-    currencyOne: document.getElementById('currency-one'),
-    currencyTwo: document.getElementById('currency-two'),
+    currencies: document.querySelectorAll('#currency-one, #currency-two'),
     amountOne: document.getElementById('amount-one'),
     amountTwo: document.getElementById('amount-two'),
-    rate: document.getElementById('rate'),
     swap: document.getElementById('swap'),
+    rate: document.getElementById('rate'),
 };
+const test = () => {
+    console.log('hi');
+};
+const getRates = () => {
+};
+const swapCurrencies = () => {
+};
+const updateUI = () => {
+};
+elements.currencies.forEach(currency => currency.addEventListener('input', getRates));
+elements.swap.addEventListener('click', swapCurrencies);
+elements.amountOne.addEventListener('input', updateUI);
--- exchange-rate/app.ts
+++ exchange-rate/app.ts
@@ -1,16 +1,44 @@
 // dom elements
 const elements = {
-  currencyOne: document.getElementById('currency-one'),
-  currencyTwo: document.getElementById('currency-two'),
-  amountOne: document.getElementById('amount-one'),
-  amountTwo: document.getElementById('amount-two'),
+  // currencyOne: document.getElementById('currency-one'),
+  // currencyTwo: document.getElementById('currency-two'),
+  currencies: document.querySelectorAll(
+    '#currency-one, #currency-two'
+  ) as NodeList,
+  amountOne: document.getElementById('amount-one') as HTMLInputElement,
+  amountTwo: document.getElementById('amount-two') as HTMLInputElement,
 
-  rate: document.getElementById('rate'),
-  swap: document.getElementById('swap'),
+  swap: document.getElementById('swap') as HTMLButtonElement,
+  rate: document.getElementById('rate') as HTMLDivElement,
 };
 
 // b3e9568b251b29fd43e5e545
 
 // functions
+const test = () => {
+  console.log('hi');
+};
+
+const getRates = () => {
+  //
+};
+
+const swapCurrencies = () => {
+  //
+};
+
+const updateUI = () => {
+  //
+};
 
 // event listeners
+// selecting currencies
+elements.currencies.forEach(currency =>
+  currency.addEventListener('input', getRates)
+);
+
+// swapping currencies
+elements.swap.addEventListener('click', swapCurrencies);
+
+// increasing currency
+elements.amountOne.addEventListener('input', updateUI);
