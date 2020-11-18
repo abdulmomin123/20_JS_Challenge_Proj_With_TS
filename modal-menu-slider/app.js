f0584f6495f72b546029f7f9e3ff9ff2cd43a384
Abdul Momin <abdulmomin114477@gmail.com>
2020-11-18 09:21:16 +0600

    all

---
+++ modal-menu-slider/app.js
@@ -0,0 +1,6 @@
+"use strict";
+const toggle = document.getElementById('toggle');
+const closeM = document.getElementById('close');
+const openM = document.getElementById('open');
+const modal = document.getElementById('modal');
+const navbar = document.getElementById('navbar');
