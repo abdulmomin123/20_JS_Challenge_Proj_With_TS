1273b9232d697e5e11b4e5cfd744a55bbfc33166
Abdul Momin <abdulmomin114477@gmail.com>
2020-11-18 20:51:11 +0600

    all

---
+++ hangman/app.js
@@ -0,0 +1,12 @@
+"use strict";
+const elements = {
+    wordEl: document.getElementById('word'),
+    wrongLettersEl: document.getElementById('wrong-letters'),
+    playAgainBtn: document.getElementById('play-button'),
+    popup: document.getElementById('popup-container'),
+    notification: document.getElementById('notification-container'),
+    finalMessage: document.getElementById('final-message'),
+    finalMessageRevealWord: document.getElementById('final-message-reveal-word'),
+    figureParts: document.querySelectorAll('.figure-part'),
+};
+const words = ['application', 'programming', 'interface', 'wizard'];
