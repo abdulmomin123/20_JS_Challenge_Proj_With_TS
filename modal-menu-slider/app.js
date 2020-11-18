2d3d13f6823cf3cfe1995d7f6055bf5f1c39511e
Abdul Momin <abdulmomin114477@gmail.com>
2020-11-18 14:19:07 +0600

    all

--- modal-menu-slider/app.js
+++ modal-menu-slider/app.js
@@ -1,6 +1,26 @@
 "use strict";
 const toggle = document.getElementById('toggle');
-const closeM = document.getElementById('close');
-const openM = document.getElementById('open');
+const closeBtn = document.getElementById('close');
+const openBtn = document.getElementById('open');
 const modal = document.getElementById('modal');
 const navbar = document.getElementById('navbar');
+function closeNavbar(e) {
+    if (document.body.classList.contains('show-nav') &&
+        e.target !== toggle &&
+        !toggle.contains(e.target) &&
+        e.target !== navbar &&
+        !navbar.contains(e.target)) {
+        document.body.classList.toggle('show-nav');
+        document.body.removeEventListener('click', closeNavbar);
+    }
+    else if (!document.body.classList.contains('show-nav')) {
+        document.body.removeEventListener('click', closeNavbar);
+    }
+}
+toggle.addEventListener('click', () => {
+    document.body.classList.toggle('show-nav');
+    document.body.addEventListener('click', closeNavbar);
+});
+openBtn.addEventListener('click', () => modal.classList.add('show-modal'));
+closeBtn.addEventListener('click', () => modal.classList.remove('show-modal'));
+window.addEventListener('click', e => e.target == modal ? modal.classList.remove('show-modal') : false);
--- modal-menu-slider/app.ts
+++ modal-menu-slider/app.ts
@@ -1,5 +1,40 @@
-const toggle = document.getElementById('toggle');
-const closeM = document.getElementById('close');
-const openM = document.getElementById('open');
-const modal = document.getElementById('modal');
-const navbar = document.getElementById('navbar');
+const toggle = document.getElementById('toggle')!;
+const closeBtn = document.getElementById('close')!;
+const openBtn = document.getElementById('open')!;
+const modal = document.getElementById('modal')!;
+const navbar = document.getElementById('navbar')!;
+
+// This function closes navbar if user clicks anywhere outside of navbar once it's opened
+// Does not leave unused event listeners on
+// It's messy, but it works
+function closeNavbar(e: Event) {
+  if (
+    document.body.classList.contains('show-nav') &&
+    e.target !== toggle &&
+    !toggle.contains(e.target as any) &&
+    e.target !== navbar &&
+    !navbar.contains(e.target as any)
+  ) {
+    document.body.classList.toggle('show-nav');
+    document.body.removeEventListener('click', closeNavbar);
+  } else if (!document.body.classList.contains('show-nav')) {
+    document.body.removeEventListener('click', closeNavbar);
+  }
+}
+
+// Toggle nav
+toggle.addEventListener('click', () => {
+  document.body.classList.toggle('show-nav');
+  document.body.addEventListener('click', closeNavbar);
+});
+
+// Show modal
+openBtn.addEventListener('click', () => modal.classList.add('show-modal'));
+
+// Hide modal
+closeBtn.addEventListener('click', () => modal.classList.remove('show-modal'));
+
+// Hide modal on outside click
+window.addEventListener('click', e =>
+  e.target == modal ? modal.classList.remove('show-modal') : false
+);
