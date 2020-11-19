96a05589f09737b2b9b38567bb6bd9cb79ce7e10
Abdul Momin <abdulmomin114477@gmail.com>
2020-11-19 21:03:40 +0600

    all

---
+++ meal-finder/app.js
@@ -0,0 +1,9 @@
+"use strict";
+const elements = {
+    search: document.getElementById('search'),
+    submit: document.getElementById('submit'),
+    random: document.getElementById('random'),
+    mealsEl: document.getElementById('meals'),
+    resultHeading: document.getElementById('result-heading'),
+    single_mealEl: document.getElementById('single-meal'),
+};
