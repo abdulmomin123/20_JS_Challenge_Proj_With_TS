4588d740a0b63a107dec3a6f6fe76bd4873ebdc9
Abdul Momin <abdulmomin114477@gmail.com>
2020-11-19 21:19:38 +0600

    all

--- meal-finder/app.js
+++ meal-finder/app.js
@@ -7,6 +7,14 @@ const elements = {
     resultHeading: document.getElementById('result-heading'),
     single_mealEl: document.getElementById('single-meal'),
 };
-const test = () => {
+const test = (e) => {
+    e.preventDefault();
+    console.log('hi');
+};
+const clearInput = () => {
+    elements.search.value = '';
+};
+const displaySearchTerm = () => {
 };
 elements.submit.addEventListener('submit', test);
+elements.random.addEventListener('click', test);
--- meal-finder/app.ts
+++ meal-finder/app.ts
@@ -3,15 +3,29 @@ const elements = {
   search: document.getElementById('search') as HTMLInputElement,
   submit: document.getElementById('submit') as HTMLFormElement,
   random: document.getElementById('random') as HTMLButtonElement,
-  mealsEl: document.getElementById('meals'),
-  resultHeading: document.getElementById('result-heading'),
-  single_mealEl: document.getElementById('single-meal'),
+  mealsEl: document.getElementById('meals') as HTMLDivElement,
+  resultHeading: document.getElementById('result-heading') as HTMLDivElement,
+  single_mealEl: document.getElementById('single-meal') as HTMLDivElement,
 };
 
 // functions
-const test = () => {
+const test = (e: Event) => {
+  e.preventDefault();
+
+  console.log('hi');
+};
+
+const clearInput = () => {
+  elements.search.value = '';
+};
+
+const displaySearchTerm = () => {
   //
 };
 
 // event listeners
+// the search handler
 elements.submit.addEventListener('submit', test);
+
+// random meal handler
+elements.random.addEventListener('click', test);
