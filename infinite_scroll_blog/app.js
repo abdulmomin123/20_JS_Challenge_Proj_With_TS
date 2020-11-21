9e6de8aa9ccf6f3fe4895a93c092db2614bb051f
Abdul Momin <abdulmomin114477@gmail.com>
2020-11-21 15:31:39 +0600

    all

---
+++ infinite_scroll_blog/app.js
@@ -0,0 +1,6 @@
+"use strict";
+const postsContainer = document.getElementById('posts-container');
+const loading = document.querySelector('.loader');
+const filter = document.getElementById('filter');
+let limit = 5;
+let page = 1;
