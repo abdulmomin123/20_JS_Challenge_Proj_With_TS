f28539bd5f7ac094d9a48cd63516a8f1287e0eaf
Abdul Momin <abdulmomin114477@gmail.com>
2020-11-25 09:34:23 +0600

    all

---
+++ new-year-countdown/app.ts
@@ -0,0 +1,12 @@
+// dom elements
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+const countdown = document.getElementById('countdown');
+const year = document.getElementById('year');
+const loading = document.getElementById('loading');
+
+// global variables
+
+// functions
