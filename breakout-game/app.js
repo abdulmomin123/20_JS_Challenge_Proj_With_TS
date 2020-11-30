dfc4c2b612fea59c4a86f12484f00c3d7eea5218
Abdul Momin <abdulmomin114477@gmail.com>
2020-11-30 15:34:32 +0600

    all

--- breakout-game/app.js
+++ breakout-game/app.js
@@ -15,144 +15,29 @@ let ballDirectionX = 'right';
 let ballDirectionY = 'top';
 let ballX = elements.canvas.width / 3;
 let ballY = elements.canvas.height - 150;
-const drawBricks = (config) => {
-    const { startX, startY, brickHeight, bricksPerRow, totalRows, spaceBetweenBrick, } = config;
-    let startingPointX = startX;
-    let startingPointY = startY;
-    const totalWidth = elements.canvas.width - startX * 2;
-    const widthWithoutSpace = totalWidth - (bricksPerRow - 1) * spaceBetweenBrick;
-    const singleBrickWidth = widthWithoutSpace / bricksPerRow;
-    for (let i = 0; i < totalRows; i++) {
-        for (let j = 0; j < bricksPerRow; j++) {
-            ctx.fillRect(startingPointX, startingPointY, 70, brickHeight);
-            startingPointX += singleBrickWidth + spaceBetweenBrick;
-        }
-        startingPointX = startX;
-        startingPointY += brickHeight + spaceBetweenBrick;
+class Ball {
+    constructor(x, y, radius, speed, dx, dy, visible) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speed = speed;
+        this.dx = dx;
+        this.dy = dy;
+        this.visible = visible;
     }
-};
-const drawBall = (config) => {
-    const { ballX, ballY, radius } = config;
-    ctx.beginPath();
-    ctx.arc(ballX, ballY, radius, 1, 8);
-    ctx.fill();
-};
-const drawBar = (config) => {
-    const { barX, barY, width, height } = config;
-    ctx.clearRect(0, barY, elements.canvas.width, height);
-    ctx.fillRect(barX, barY, width, height);
-};
-const drawScore = () => {
-    ctx.fillText(`Score: ${score}`, elements.canvas.width - 140, 35);
-};
-const moveBar = () => {
-    if ((barX <= 0 && moveDirection < 0) ||
-        (barX >= elements.canvas.width - 80 && moveDirection > 0))
-        return;
-    barX += moveDirection;
-    drawBar({
-        barX,
-        barY: elements.canvas.height - 10 * 2,
-        width: 80,
-        height: 10,
-    });
-};
-const moveBall = () => {
-    if (ballY <= 10 && ballDirectionX === 'right') {
-        ballDirectionX = 'right';
-        ballDirectionY = 'bottom';
+    move(x, y) {
+        this.x += x;
+        this.y += y;
     }
-    else if (ballY <= 10 && ballDirectionX === 'left') {
-        ballDirectionX = 'left';
-        ballDirectionY = 'bottom';
+}
+class Bar {
+    constructor(x, y, w, h, speed, dx, visible) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.speed = speed;
+        this.dx = dx;
+        this.visible = visible;
     }
-    else if (ballX <= 10 && ballDirectionY === 'top') {
-        ballDirectionX = 'right';
-        ballDirectionY = 'top';
-    }
-    else if (ballX <= 10 && ballDirectionY === 'bottom') {
-        ballDirectionX = 'right';
-        ballDirectionY = 'bottom';
-    }
-    else if (ballY >= elements.canvas.height - 30 &&
-        ballDirectionX === 'right') {
-        ballDirectionX = 'right';
-        ballDirectionY = 'top';
-    }
-    else if (ballY >= elements.canvas.height - 30 &&
-        ballDirectionX === 'left') {
-        ballDirectionX = 'left';
-        ballDirectionY = 'top';
-    }
-    else if (ballX >= elements.canvas.width - 10 && ballDirectionY === 'top') {
-        ballDirectionX = 'left';
-        ballDirectionY = 'top';
-    }
-    else if (ballX >= elements.canvas.width - 10 &&
-        ballDirectionY === 'bottom') {
-        ballDirectionX = 'left';
-        ballDirectionY = 'bottom';
-    }
-    if (ballDirectionY === 'top' && ballDirectionX === 'right') {
-        ballX += 4;
-        ballY -= 4;
-    }
-    else if (ballDirectionY === 'top' && ballDirectionX === 'left') {
-        ballX -= 4;
-        ballY -= 4;
-    }
-    else if (ballDirectionY === 'bottom' && ballDirectionX === 'right') {
-        ballX += 4;
-        ballY += 4;
-    }
-    else if (ballDirectionY === 'bottom' && ballDirectionX === 'left') {
-        ballX -= 4;
-        ballY += 4;
-    }
-};
-const drawAll = () => {
-    ctx.clearRect(0, 0, elements.canvas.width, elements.canvas.height);
-    drawBricks({
-        startX: 45,
-        startY: 60,
-        brickHeight: 20,
-        bricksPerRow: 9,
-        totalRows: 5,
-        spaceBetweenBrick: 10,
-    });
-    drawBall({
-        ballX,
-        ballY,
-        radius: 10,
-    });
-    drawBar({
-        barX,
-        barY: elements.canvas.height - 10 * 2,
-        width: 80,
-        height: 10,
-    });
-    drawScore();
-};
-const updateCanvas = () => {
-    moveBall();
-    moveBar();
-    drawAll();
-    requestAnimationFrame(updateCanvas);
-};
-updateCanvas();
-document.addEventListener('keydown', e => {
-    const key = e.keyCode;
-    if (key !== 37 && key !== 39 && key !== 65 && key !== 68)
-        return;
-    if (key === 37 || key === 65) {
-        moveDirection = -8;
-    }
-    else if (key === 39 || key === 68) {
-        moveDirection = 8;
-    }
-});
-document.addEventListener('keyup', () => {
-    moveDirection = 0;
-});
-elements.rulesBtn.addEventListener('click', () => elements.rules.classList.add('show'));
-elements.closeBtn.addEventListener('click', () => elements.rules.classList.remove('show'));
+}
--- breakout-game/app.ts
+++ breakout-game/app.ts
@@ -11,6 +11,7 @@ const ctx = elements.canvas.getContext('2d') as CanvasRenderingContext2D;
 ctx.fillStyle = '#0095dd';
 ctx.font = '20px Arial';
 
+// score
 let score = 0;
 
 // current positon of the bar
@@ -22,6 +23,39 @@ let ballDirectionX: 'left' | 'right' = 'right';
 let ballDirectionY: 'top' | 'bottom' = 'top';
 let ballX = elements.canvas.width / 3;
 let ballY = elements.canvas.height - 150;
+
+class Ball {
+  constructor(
+    public x: number,
+    public y: number,
+    public radius: number,
+    public speed: number,
+    public dx: number,
+    public dy: number,
+    public visible: boolean
+  ) {
+    //
+  }
+
+  move(x: number, y: number) {
+    this.x += x;
+    this.y += y;
+  }
+}
+
+class Bar {
+  constructor(
+    public x: number,
+    public y: number,
+    public w: number,
+    public h: number,
+    public speed: number,
+    public dx: number,
+    public visible: boolean
+  ) {
+    //
+  }
+}
 
 interface Bricks {
   startX: number;
@@ -39,196 +73,190 @@ interface Bar {
   height: number;
 }
 
-interface Ball {
-  ballX: number;
-  ballY: number;
-  radius: number;
-}
+// // functions
+// const drawBricks = (config: Bricks) => {
+//   const {
+//     startX,
+//     startY,
+//     brickHeight,
+//     bricksPerRow,
+//     totalRows,
+//     spaceBetweenBrick,
+//   } = config;
 
-// functions
-const drawBricks = (config: Bricks) => {
-  const {
-    startX,
-    startY,
-    brickHeight,
-    bricksPerRow,
-    totalRows,
-    spaceBetweenBrick,
-  } = config;
+//   let startingPointX = startX;
+//   let startingPointY = startY;
 
-  let startingPointX = startX;
-  let startingPointY = startY;
+//   const totalWidth = elements.canvas.width - startX * 2;
+//   const widthWithoutSpace = totalWidth - (bricksPerRow - 1) * spaceBetweenBrick;
 
-  const totalWidth = elements.canvas.width - startX * 2;
-  const widthWithoutSpace = totalWidth - (bricksPerRow - 1) * spaceBetweenBrick;
+//   const singleBrickWidth = widthWithoutSpace / bricksPerRow;
 
-  const singleBrickWidth = widthWithoutSpace / bricksPerRow;
+//   for (let i = 0; i < totalRows; i++) {
+//     for (let j = 0; j < bricksPerRow; j++) {
+//       ctx.fillRect(startingPointX, startingPointY, 70, brickHeight);
+//       startingPointX += singleBrickWidth + spaceBetweenBrick;
+//     }
 
-  for (let i = 0; i < totalRows; i++) {
-    for (let j = 0; j < bricksPerRow; j++) {
-      ctx.fillRect(startingPointX, startingPointY, 70, brickHeight);
-      startingPointX += singleBrickWidth + spaceBetweenBrick;
-    }
+//     startingPointX = startX;
+//     startingPointY += brickHeight + spaceBetweenBrick;
+//   }
+// };
 
-    startingPointX = startX;
-    startingPointY += brickHeight + spaceBetweenBrick;
-  }
-};
+// const drawBall = (config: Ball) => {
+//   const { ballX, ballY, radius } = config;
 
-const drawBall = (config: Ball) => {
-  const { ballX, ballY, radius } = config;
+//   ctx.beginPath();
+//   ctx.arc(ballX, ballY, radius, 1, 8);
+//   ctx.fill();
+// };
 
-  ctx.beginPath();
-  ctx.arc(ballX, ballY, radius, 1, 8);
-  ctx.fill();
-};
+// const drawBar = (config: Bar) => {
+//   const { barX, barY, width, height } = config;
 
-const drawBar = (config: Bar) => {
-  const { barX, barY, width, height } = config;
+//   ctx.clearRect(0, barY, elements.canvas.width, height);
 
-  ctx.clearRect(0, barY, elements.canvas.width, height);
+//   ctx.fillRect(barX, barY, width, height);
+// };
 
-  ctx.fillRect(barX, barY, width, height);
-};
+// const drawScore = () => {
+//   ctx.fillText(`Score: ${score}`, elements.canvas.width - 140, 35);
+// };
 
-const drawScore = () => {
-  ctx.fillText(`Score: ${score}`, elements.canvas.width - 140, 35);
-};
+// const moveBar = () => {
+//   if (
+//     (barX <= 0 && moveDirection < 0) ||
+//     (barX >= elements.canvas.width - 80 && moveDirection > 0)
+//   )
+//     return;
 
-const moveBar = () => {
-  if (
-    (barX <= 0 && moveDirection < 0) ||
-    (barX >= elements.canvas.width - 80 && moveDirection > 0)
-  )
-    return;
+//   barX += moveDirection;
 
-  barX += moveDirection;
+//   drawBar({
+//     barX,
+//     barY: elements.canvas.height - 10 * 2,
+//     width: 80,
+//     height: 10,
+//   });
+// };
 
-  drawBar({
-    barX,
-    barY: elements.canvas.height - 10 * 2,
-    width: 80,
-    height: 10,
-  });
-};
+// const moveBall = () => {
+//   if (ballY <= 10 && ballDirectionX === 'right') {
+//     ballDirectionX = 'right';
+//     ballDirectionY = 'bottom';
+//   } else if (ballY <= 10 && ballDirectionX === 'left') {
+//     ballDirectionX = 'left';
+//     ballDirectionY = 'bottom';
+//   } else if (ballX <= 10 && ballDirectionY === 'top') {
+//     ballDirectionX = 'right';
+//     ballDirectionY = 'top';
+//   } else if (ballX <= 10 && ballDirectionY === 'bottom') {
+//     ballDirectionX = 'right';
+//     ballDirectionY = 'bottom';
+//   } else if (
+//     ballY >= elements.canvas.height - 30 &&
+//     ballDirectionX === 'right'
+//   ) {
+//     ballDirectionX = 'right';
+//     ballDirectionY = 'top';
+//   } else if (
+//     ballY >= elements.canvas.height - 30 &&
+//     ballDirectionX === 'left'
+//   ) {
+//     ballDirectionX = 'left';
+//     ballDirectionY = 'top';
+//   } else if (ballX >= elements.canvas.width - 10 && ballDirectionY === 'top') {
+//     ballDirectionX = 'left';
+//     ballDirectionY = 'top';
+//   } else if (
+//     ballX >= elements.canvas.width - 10 &&
+//     ballDirectionY === 'bottom'
+//   ) {
+//     ballDirectionX = 'left';
+//     ballDirectionY = 'bottom';
+//   }
 
-const moveBall = () => {
-  if (ballY <= 10 && ballDirectionX === 'right') {
-    ballDirectionX = 'right';
-    ballDirectionY = 'bottom';
-  } else if (ballY <= 10 && ballDirectionX === 'left') {
-    ballDirectionX = 'left';
-    ballDirectionY = 'bottom';
-  } else if (ballX <= 10 && ballDirectionY === 'top') {
-    ballDirectionX = 'right';
-    ballDirectionY = 'top';
-  } else if (ballX <= 10 && ballDirectionY === 'bottom') {
-    ballDirectionX = 'right';
-    ballDirectionY = 'bottom';
-  } else if (
-    ballY >= elements.canvas.height - 30 &&
-    ballDirectionX === 'right'
-  ) {
-    ballDirectionX = 'right';
-    ballDirectionY = 'top';
-  } else if (
-    ballY >= elements.canvas.height - 30 &&
-    ballDirectionX === 'left'
-  ) {
-    ballDirectionX = 'left';
-    ballDirectionY = 'top';
-  } else if (ballX >= elements.canvas.width - 10 && ballDirectionY === 'top') {
-    ballDirectionX = 'left';
-    ballDirectionY = 'top';
-  } else if (
-    ballX >= elements.canvas.width - 10 &&
-    ballDirectionY === 'bottom'
-  ) {
-    ballDirectionX = 'left';
-    ballDirectionY = 'bottom';
-  }
+//   // movement
+//   if (ballDirectionY === 'top' && ballDirectionX === 'right') {
+//     ballX += 4;
+//     ballY -= 4;
+//   } else if (ballDirectionY === 'top' && ballDirectionX === 'left') {
+//     ballX -= 4;
+//     ballY -= 4;
+//   } else if (ballDirectionY === 'bottom' && ballDirectionX === 'right') {
+//     ballX += 4;
+//     ballY += 4;
+//   } else if (ballDirectionY === 'bottom' && ballDirectionX === 'left') {
+//     ballX -= 4;
+//     ballY += 4;
+//   }
+// };
 
-  // movement
-  if (ballDirectionY === 'top' && ballDirectionX === 'right') {
-    ballX += 4;
-    ballY -= 4;
-  } else if (ballDirectionY === 'top' && ballDirectionX === 'left') {
-    ballX -= 4;
-    ballY -= 4;
-  } else if (ballDirectionY === 'bottom' && ballDirectionX === 'right') {
-    ballX += 4;
-    ballY += 4;
-  } else if (ballDirectionY === 'bottom' && ballDirectionX === 'left') {
-    ballX -= 4;
-    ballY += 4;
-  }
-};
+// const drawAll = () => {
+//   ctx.clearRect(0, 0, elements.canvas.width, elements.canvas.height);
 
-const drawAll = () => {
-  ctx.clearRect(0, 0, elements.canvas.width, elements.canvas.height);
+//   drawBricks({
+//     startX: 45,
+//     startY: 60,
+//     brickHeight: 20,
+//     bricksPerRow: 9,
+//     totalRows: 5,
+//     spaceBetweenBrick: 10,
+//   });
 
-  drawBricks({
-    startX: 45,
-    startY: 60,
-    brickHeight: 20,
-    bricksPerRow: 9,
-    totalRows: 5,
-    spaceBetweenBrick: 10,
-  });
+//   drawBall({
+//     ballX,
+//     ballY,
+//     radius: 10,
+//   });
 
-  drawBall({
-    ballX,
-    ballY,
-    radius: 10,
-  });
+//   drawBar({
+//     barX,
+//     barY: elements.canvas.height - 10 * 2,
+//     width: 80,
+//     height: 10,
+//   });
 
-  drawBar({
-    barX,
-    barY: elements.canvas.height - 10 * 2,
-    width: 80,
-    height: 10,
-  });
+//   drawScore();
+// };
 
-  drawScore();
-};
+// const updateCanvas = () => {
+//   // move the ball & bar
+//   moveBall();
+//   moveBar();
 
-const updateCanvas = () => {
-  // move the ball & bar
-  moveBall();
-  moveBar();
+//   // draw everything
+//   drawAll();
 
-  // draw everything
-  drawAll();
+//   // do it again
+//   requestAnimationFrame(updateCanvas);
+// };
 
-  // do it again
-  requestAnimationFrame(updateCanvas);
-};
+// updateCanvas();
 
-updateCanvas();
+// // event listeners
+// // move the bar left or right
+// document.addEventListener('keydown', e => {
+//   const key = e.keyCode;
 
-// event listeners
-// move the bar left or right
-document.addEventListener('keydown', e => {
-  const key = e.keyCode;
+//   if (key !== 37 && key !== 39 && key !== 65 && key !== 68) return;
 
-  if (key !== 37 && key !== 39 && key !== 65 && key !== 68) return;
+//   if (key === 37 || key === 65) {
+//     moveDirection = -8;
+//   } else if (key === 39 || key === 68) {
+//     moveDirection = 8;
+//   }
+// });
 
-  if (key === 37 || key === 65) {
-    moveDirection = -8;
-  } else if (key === 39 || key === 68) {
-    moveDirection = 8;
-  }
-});
+// // reset the movedirection
+// document.addEventListener('keyup', () => {
+//   moveDirection = 0;
+// });
 
-// reset the movedirection
-document.addEventListener('keyup', () => {
-  moveDirection = 0;
-});
-
-// Rules and close event handlers
-elements.rulesBtn.addEventListener('click', () =>
-  elements.rules.classList.add('show')
-);
-elements.closeBtn.addEventListener('click', () =>
-  elements.rules.classList.remove('show')
-);
+// // Rules and close event handlers
+// elements.rulesBtn.addEventListener('click', () =>
+//   elements.rules.classList.add('show')
+// );
+// elements.closeBtn.addEventListener('click', () =>
+//   elements.rules.classList.remove('show')
+// );
